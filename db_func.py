5dfeb1125612201123d03c282ce3c24f1576dda9
reannu123 <reannumon123@gmail.com>
2022-03-07 21:19:06 +0800

    Cleaned code

--- db_func.py
+++ db_func.py
@@ -24,16 +24,24 @@ collection = db["test_collection"]
 
 def make_server_collection(guild):
     collection = db[str(guild.id)]
+
+    # Check if guild already in database, if not, create new collection with 
     if str(guild.id) not in db.list_collection_names():
-        collection.insert_many([{"_id":guild.id,"type":"Server Data","name":guild.name,"time":datetime.now(),"timezone":str(timedelta(hours=8))}])
-        return False, 0 ,0
+        collection.insert_many([
+            {"_id":guild.id,
+            "type":"Server Data",
+            "name":guild.name,
+            "time_created":datetime.now(),
+            "timezone":str(timedelta(hours=8)),
+            }])
+        return False, 0
+
+    # Send guild data back to the bot
     else:
         server_data = collection.find_one({"_id":guild.id})
+        # Covnert time to server timezone
         t = datetime.strptime(server_data['timezone'],"%H:%M:%S")
         delta = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
-        local_time = server_data['time']+delta
-        print(server_data['time'])
-        print(local_time)
-        print(delta)
-        return True,local_time,server_data
+        local_time = server_data['time_created'] + delta
+        return True, local_time
     
