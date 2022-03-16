2d045e4d8bb21e3447a2c5082ad5f62611f90a45
reannu123 <reannumon123@gmail.com>
2022-03-17 00:36:37 +0800

    Update db_func.py
    Implemented AgendaTable for ID translation.
    Added list_agenda to return all agenda.
    Improved find_agenda to accept ID.

--- db_func.py
+++ db_func.py
@@ -62,6 +62,13 @@ def make_server_collection(guild):
             "name":guild.name,
             "time_created":datetime.utcnow(),
             "timezone":[timedelta(hours=8).days, timedelta(hours=8).seconds],
+            },{
+            "Type":"AgendaTable",
+            "Task":[],
+            "Project":[],
+            "Reminder":[],
+            "Meeting":[],
+	
             }])
         return False, 0
 
@@ -104,13 +111,25 @@ def add_agenda(ctx, AgendaType, args):
                                 "AuthorID":authorID, 
                                 "Assigned":authorID,
                                 "Args":args})
-        message = f"{AgendaType} added successfullly!"
+        currentAgenda = collection.find_one({ "Type":"Agenda",
+                                "AgendaType":AgendaType,
+                                "AuthorID":authorID, 
+                                "Assigned":authorID,
+                                "Args":args})
+        table = collection.find_one({   "Type":"AgendaTable"
+        })
+
+        table[AgendaType].append(currentAgenda['_id'])
+        collection.update_one({"Type":"AgendaTable"},{'$set':{AgendaType:table[AgendaType]}})
+
+        message = f"{AgendaType} added successfully!"
     else:
-        message = f"Data already exists. \nTo update, use ;update <{AgendaType} name>"
+        message = f"Data already exists. \nTo update, use ;update <agenda> command."
     return message
 
 # Function that enters agenda data into the database, requires AgendaType, and the arguments entered
 def find_agenda(ctx, AgendaType, args):
+
   # Only proceed if server is registered
     if str(ctx.guild.id) not in db.list_collection_names():
         return "Server not yet registered in the database. Please register with the command ;server_register"
@@ -120,26 +139,86 @@ def find_agenda(ctx, AgendaType, args):
     # Separate args into useful
     args = [i.strip() for i in args]
     args = [i.strip() for i in args if i!='']
+    
+    authorID = ctx.author.id
+    authorName = ctx.author.name
+    authorRoles = [i.id for i in ctx.author.roles]
+
+    table = collection.find_one({"Type":"AgendaTable"})
+    AgendaIDList = table[AgendaType]
+
+
+    if (int(args[0])>len(AgendaIDList)-1):
+        return "Sorry, that {} does not yet exist.".format(AgendaType)
+
+    # Data Querying
+    
+    data = collection.find_one({"_id":AgendaIDList[int(args[0])]})
+    members = {}
+
+    # Temporarily store member display names
+    for i in ctx.guild.members:
+        members[i.id] = i.display_name
+
+    # Check if data of agenda exists
+
+    if data == None:
+        return "Sorry, that {} does not yet exist.".format(AgendaType)
+    else:
+        
+
+        # Data Formatting
+
+        
+        message = ""
+        for i in data:
+            # If attribute is AuthorID and Assigned, substitute to Display name
+            if(str(i) == "AuthorID" or str(i) == "Assigned"):
+                data[i] = members[data[i]]
+
+            # Do not include attributes _id and Type in message
+            Exclude = ["Type", "_id", "Args"]
+            if(str(i) not in Exclude):
+                toSend = data[i]
+                if(i == "AuthorID"):
+                    i = "Made by"
+                elif(i == "Assigned"):
+                    i = "Assigned to"
+                elif(i == "AgendaType"):
+                    i = "Type"
+                message += str(i) + ": " +str(toSend) + "\n"
+        args = data['Args']
+        for i in range(len(args)):
+            attributes = ["Description: ","Date: ","Time: "]
+            message += attributes[i] + args[i] + "\n"
+
+        message = "Here's what I found:\n\n" + message
+    return message
+
+def list_agenda(ctx, AgendaType):
+    # Only proceed if server is registered
+    if str(ctx.guild.id) not in db.list_collection_names():
+        return "Server not yet registered in the database. Please register with the command ;server_register"
+  # The collection (like a sub-database) will depend on the server/guild id
+    collection = db[str(ctx.guild.id)]
+
 
     authorID = ctx.author.id
     authorName = ctx.author.name
     authorRoles = [i.id for i in ctx.author.roles]
 
-    data = collection.find_one({"Type":"Agenda",
-                                "AgendaType":AgendaType, 
-                                "AuthorID":authorID, 
-                                "Assigned":authorID,
-                                "Args":args})
-    members = {}
-    for i in ctx.guild.members:
-        members[i.id] = i.display_name
-    if data == None:
-        message = f"Sorry, that {AgendaType} does not yet exist."
+
+    # Data Querying
+    table = collection.find_one({"Type":"AgendaTable"})
+    AgendaIDList = table[AgendaType]
+    AgendaList = []
+    message = ""
+    if(len(AgendaIDList) > 0):
+        for i in AgendaIDList:
+            agenda = collection.find_one({"_id":i})["Args"]
+            print(agenda)
+            message += AgendaType + " " + str(AgendaIDList.index(i)) + "\t"+ agenda[0] + "\n"
+            AgendaList.append(agenda[0])
     else:
-        message = ""
-        for i in data:
-            if(str(i) == "AuthorID" or str(i) == "Assigned"):
-                data[i] = members[data[i]]
-            message += str(i) + ": " +str(data[i]) + "\n"
-        message = "Here's what I found:\n" + message 
+        message = "Nothing found."
     return message
